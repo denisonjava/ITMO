f28816b5ce0ef16fc9de0a48a9bfae549ad4f615
denisonjava <deku@mail.ru>
2026-01-17 22:31:54 +0300

    Выполнено задание Project 1_2B

---
+++ Python/Lesson №2/Project 1_2B.py
@@ -0,0 +1,5 @@
+food = ['fruits','meat','vegetables','beer','tomatoes','rice','water','bread','milk','coffee']
+price = [10,40,15,32,23,76,45,34,655,4]
+code = [777,543,234,234,543,876,345,123,546,843]
+info = list(zip(food,price,code))
+print(info)
